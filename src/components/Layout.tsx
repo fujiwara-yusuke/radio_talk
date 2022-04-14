2a31bb507439819152831824907becc6433ddc27
fujiwara-yusuke <tomosuke8.guitar@gmail.com>
2022-04-14 23:35:17 +0900

    スペース修正

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,27 +3,24 @@ import Link from 'next/link';
 import { ReactNode } from 'react';
 
 type Props = {
-    children?: ReactNode;
+  children?: ReactNode;
 };
 
 const Layout = ({ children }: Props) => {
-    return (
-        <div>
-            <Head>
-                <title>MyTemplate</title>
-            </Head>
-
-            <header className=''>
-                <Link href='/'>
-                    <a>Home</a>
-                </Link>
-            </header>
-
-            <div className='content'>{children}</div>
-
-            <footer className=''></footer>
-        </div>
-    );
+  return (
+    <div>
+      <Head>
+        <title>MyTemplate</title>
+      </Head>
+      <header className=''>
+        <Link href='/'>
+          <a>Home</a>
+        </Link>
+      </header>
+      <div className='content'>{children}</div>
+      <footer className=''></footer>
+    </div>
+  );
 };
 
 export default Layout;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,11 +3,11 @@ import Layout from 'components/Layout';
 import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
-    return (
-        <Layout>
-            <Component {...pageProps} />
-        </Layout>
-    );
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
 export default MyApp;
