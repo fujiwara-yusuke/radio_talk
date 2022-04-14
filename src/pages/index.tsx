9e7da448c833eae5049b9e08c5d1b2a5143ad082
fujiwara-yusuke <tomosuke8.guitar@gmail.com>
2022-04-15 04:22:45 +0900

    細分化したので削除

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,14 @@
 import type { NextPage } from 'next';
-import styled from 'styled-components';
 import PostForm from 'components/PostForm';
-import TopicsSlider from 'components/TopicsSlider';
+import Topics from 'components/TopicsSlider';
 
 const Home: NextPage = () => {
   return (
     <>
-      <TopicsSlider/>
+      <Topics/>
       <PostForm/>
     </>
   );
 };
 
-const SliderWrapper = styled.div`
-  .slider{
-    background: #FFF;
-    color: #000;
-    width: 95%;
-    border-radius: 5px;
-    height: 250px;
-    margin: auto;
-  }
-`
 export default Home;
